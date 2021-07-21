38a7ffff7dcce28bf4c655c8908c8ec1daf00e30
Konstantinos Argyriou <constarg@pm.me>
2021-07-21 04:21:48 +0300

    Almost done with manager.

--- src/manager.c
+++ src/manager.c
@@ -1,6 +1,9 @@
+#include <sys/types.h>
 #include <malloc.h>
 #include <pthread.h>
 #include <unistd.h>
+#include <string.h>
+#include <dirent.h>
 
 #include "manager.h"
 #include "../tools/configParser.h"
@@ -10,41 +13,78 @@
 
 #define PARSER_FAILED "Parser Failed"
 #define PARSE_INTERVAL_NOT_FOUND "Parse interval not found, set default"
+#define FAILED_TO_OPEN_DIR "Parser Failed to open a check dir."
 
 #define DEFAULT_PARSE_INTERVAL 5000
+#define RETRY_INTERVAL 3
 
-_Noreturn void* parse(void*);
-void* moveToDir(void*);
+_Noreturn void *parse(void *);
+_Noreturn void *moveToDir(void *);
+void checkMoveFile(char*);
+struct config *conf;
 
 void run() {
-    struct config* conf = malloc(sizeof(struct config));
+    conf = malloc(sizeof(struct config));
     pthread_t moveToDirThread;
     pthread_t parseThread;
 
-    pthread_create(&parseThread, NULL, parse, conf);
-    //pthread_create(&moveToDirThread, NULL, moveToDir, conf);
-    pthread_join(parseThread, NULL);
+    pthread_create(&parseThread, NULL, parse, NULL);
+    pthread_create(&moveToDirThread, NULL, moveToDir, NULL);
+
+    pthread_exit(NULL);
 }
 
-void* parse(void* conf) {
-    struct config* currConf = conf;
-    // parse the config file using parser.
+void *parse(void *arg) {
 
     while (TRUE) {
-        currConf = clearConfig(currConf);
+        conf = clearConfig(conf);
         if (getConfig(conf) == -1) makeLog(PARSER_FAILED, NULL, NORMAL_LOG, WARN);
 
-        if (currConf -> parseInterval == NULL) {
+        if (conf->parseInterval == NULL) {
             sleep(DEFAULT_PARSE_INTERVAL);
-            if (*(currConf -> debugLog) == 1) makeLog(PARSE_INTERVAL_NOT_FOUND, NULL, DEBUG_LOG, WARN);
+            if (*(conf->debugLog) == 1) makeLog(PARSE_INTERVAL_NOT_FOUND, NULL, DEBUG_LOG, WARN);
             continue;
         }
-        sleep(*(currConf -> parseInterval));
+
+        sleep(*(conf->parseInterval));
     }
 }
 
 
-void* moveToDir(void* conf) {
+void *moveToDir(void *arg) {
+    struct dirent *files;
+    char* currDir;
+    char* path;
+    DIR* dir;
 
+    while (TRUE) {
+        if (conf->check == NULL) {
+            sleep(RETRY_INTERVAL);
+            continue;
+        }
+        //sprintf("%d ", *(conf->checkNumber));
+        for (int currCheck = 1; strcmp(conf->check[currCheck], "[targets]") != 0; currCheck++) {
+            currDir = conf->check[currCheck];
+            dir = opendir(currDir);
 
+            if (dir == NULL) makeLog(FAILED_TO_OPEN_DIR, NULL, DEBUG_LOG, ERROR);
+
+            while ((files = readdir(dir)) != NULL) {
+
+                if (files->d_type == DT_REG) {
+                    path = malloc(sizeof(char) * (strlen(currDir) + strlen(files->d_name)));
+                    strcpy(path, currDir);
+                    strcat(path, files->d_name);
+                    // TODO move the to the desire dir.
+                    free(path);
+                }
+            }
+            closedir(dir);
+        }
+        sleep(*(conf->checkInterval));
+    }
+}
+
+void checkMoveFile(char* filepath) {
+    char* currType;
 }
