49b11832d58ea4fdeaa494ca3d78ea115facaf80
Konstantinos Argyriou <constarg@pm.me>
2021-09-04 17:34:21 +0300

    Almost done with the config parser.

--- tools/include/mem.h
+++ tools/include/mem.h
@@ -8,7 +8,7 @@
 
 #define ALLOCATE_MEM(VAR, SIZE, SIZE_OF_TYPE) do {      \
             (VAR) = calloc(SIZE, SIZE_OF_TYPE);         \
-            if ( (VAR) == NULL) {                       \
+            if ( (VAR) == NULL ) {                      \
                 make_log(FAILED_MEMORY_MSG,             \
                          OUT_OF_MEMORY,                 \
                          NORMAL_LOG,                    \
@@ -16,5 +16,17 @@
                 exit(1);                                \
             }                                           \
     } while(0)
+
+#define REALLOCATE_MEM(VAR, NEW_SIZE) do {              \
+            (VAR) = realloc((VAR), NEW_SIZE);           \
+            if ( (VAR) == NULL ) {                      \
+                make_log(FAILED_MEMORY_MSG,             \
+                         OUT_OF_MEMORY,                 \
+                         NORMAL_LOG,                    \
+                         ERROR);                        \
+                exit(1);                                \
+            }                                           \
+    } while(0)
+
 
 #endif
