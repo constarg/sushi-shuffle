93f048ca9cbca1a6ec8e25ccad95a27d7597a1a6
Constantinos Argyriou <rounnus@gmail.com>
2022-03-26 23:49:26 +0200

    add associate opt func

--- src/config.c
+++ src/config.c
@@ -1,11 +1,19 @@
 #include <malloc.h>
+#include <string.h>
 
 #include <config.h>
 
-
-static char *isolate_opt(const char *conf_buff)
+static char *issolate_opt(const char *conf_buff, const char *opt)
 {
+	// don't change the original buffer.
+	char *tmp = strdup(conf_buff);
+	char *opt_c = strstr(tmp, opt);
+	opt_c = strtok(opt_c, " "); // separete the option and the label.
+	opt_c = strtok(NULL, "\n"); // get the option.
+	opt_c = strdup(opt_c);
 
+	free(tmp);
+	return opt_c; // return value must be freed.
 }
 
 static unsigned int parse_int_opt(const char *conf_buff, const char *opt)
@@ -27,7 +35,7 @@ static inline void free_list(char *(**list))
 {
 	for (int i = 0; (*list)[i]; i++)
 		free((*list)[i]);
-	free(*list);
+	free(*list); 
 	*list = NULL;
 }
 
@@ -35,12 +43,12 @@ int parse_config(struct config *dst, const char *conf_buff)
 {
 	dst->c_options.o_check_interval = parse_int_opt(conf_buff, "check_interval");
 	dst->c_options.o_parse_interval = parse_int_opt(conf_buff, "parse_interval");
-	dst->c_options.o_debug_log = parse_int_opt(conf_buff, "debug_log") & 0x1;
-	dst->c_options.o_default_path = parse_str_opt(conf_buff, "default_path");
+	dst->c_options.o_debug_log 	= parse_int_opt(conf_buff, "debug_log") & 0x1;
+	dst->c_options.o_default_path 	= parse_str_opt(conf_buff, "default_path");
 	dst->c_options.o_enable_default = parse_int_opt(conf_buff, "enable_default") & 0x1;
-	dst->c_lists.l_check_list = parse_list(conf_buff, "[check_list]");
-	dst->c_lists.l_target_list = parse_list(conf_buff, "[target_list]");
-	dst->c_lists.l_exclude_list = parse_list(conf_buff, "[exclude_list]");
+	dst->c_lists.l_check_list 	= parse_list(conf_buff, "[check_list]");
+	dst->c_lists.l_target_list 	= parse_list(conf_buff, "[target_list]");
+	dst->c_lists.l_exclude_list 	= parse_list(conf_buff, "[exclude_list]");
 }
 
 
