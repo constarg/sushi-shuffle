b27cf4240852de282ea7f40983e86030ddd070d4
Constantinos Argyriou <rounnus@gmail.com>
2022-04-10 18:51:10 +0300

    configure threads
    - Create the two threads that will run the two procedures
    - Configure the mutex to make the whole process thread safe.

--- src/sorter.c
+++ src/sorter.c
@@ -1,7 +1,50 @@
+#include <pthread.h>
+#include <stdio.h>
+
 #include <sorter.h>
 
+static struct config *config_file;
+static pthread_mutex_t config_lock;
 
-void start_sorting(struct config *src)
+// pthread_mutex_lock(&lock);
+
+// the thread that will reparse the config file.
+static void *refresh_config(void *arg)
 {
+	
+}
 
+// the thread that will organize the files.
+static void *organize_files(void *arg)
+{
+	
+}
+
+
+
+void start_sorter(struct config *src)
+{
+	config_file = src;
+	// initalize the mutex.
+	pthread_mutex_init(&config_lock, NULL);
+
+	pthread_t refresh_t;
+	pthread_t organize_t;
+	// check if the threads has been created.
+	if (
+		pthread_create(&refresh_t, NULL, &refresh_config, NULL) != 0
+		||
+		pthread_create(&organize_t, NULL, &organize_files, NULL) != 0
+	   )
+	{
+		// TODO - Log that the threads can't be created.
+		pthread_mutex_destroy(&config_lock);
+		return;
+	}
+
+	pthread_join(refresh_t, NULL);
+	pthread_join(organize_t, NULL);
+
+	// destroy the mutex.	
+	pthread_mutex_destroy(&config_lock);
 }
